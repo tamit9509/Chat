65da56bfe316bab1415cc80c79c8dac57fecd2a0
Amit Thakur <tamit9509@gail.com>
2019-05-08 23:56:18 +0530

    -Add regexp searching in chat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { API } from './constent/content';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,14 +9,40 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'chat';
-  users;
+  users = [];
+  users2 = [];
   userLength;
-  constructor(private htttpClient: HttpClient) {}
+  search;
+  name = '';
+  constructor(private htttpClient: HttpClient, private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.search = this.fb.group({
+      name: ['']
+    });
     this.htttpClient.get(API.users).subscribe((result) => {
-      this.userLength = result.length;
-      this.users = result;
+      let i = 0;
+      while (result[i]) {
+        this.users.push(result[i]);
+        i++;
+      }
+      this.users2 = [...this.users];
+      this.userLength = this.users.length;
+    });
+  }
+
+  keyup(event) {
+    console.log(this.name);
+
+    if (!this.name) {
+      this.users = [...this.users2];
+      return;
+    }
+
+    const regex = new RegExp(`^${this.name}*`, 'gi');
+    this.users = [];
+    this.users = this.users2.filter((obj) => {
+      return regex.test(obj.name);
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, CommonModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
